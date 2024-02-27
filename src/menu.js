6b2e3c601993bed8c9299ef9a4a337f77adc3d66
mrzamin <142754418+mrzamin@users.noreply.github.com>
2024-02-27 13:01:26 -0500

    Update menu styles

--- src/menu.js
+++ src/menu.js
@@ -1,86 +1,80 @@
+import corndog from "./imgs/corndog.jpg";
+import apricots from "./imgs/apricots.jpg";
+import cauliflower from "./imgs/cauliflower.jpg";
+import salmon from "./imgs/salmon.jpg";
+import salad from "./imgs/salad.jpg";
+import beef from "./imgs/beef.jpg";
+
+class Item {
+  constructor(image, name, descr) {
+    this.image = image;
+    this.name = name;
+    this.descr = descr;
+  }
+}
+
+let menu = [
+  new Item(`${corndog}`, "Shrimp Corndog", "Bistro honey mustard"),
+  new Item(
+    `${apricots}`,
+    "Bacon-wrapped apricots",
+    "Tequila-soaked apricots, hickory smoked bacon, jalapeño kumquat, honey glaze"
+  ),
+  new Item(
+    `${cauliflower}`,
+    "Cauliflower piccata",
+    "Lemon caper aioli, pine nuts, parsley, pickled carrots, parmesan crisp"
+  ),
+  new Item(
+    `${salmon}`,
+    "Salmon Filet",
+    "Olive oil poached,white bean bacon purée, red onion poppy seed marmalade, broccolini, pickled mustard molasses glaze, smoked onion rings"
+  ),
+  new Item(
+    `${salad}`,
+    "Spinach Salad",
+    "Spinach, zucchini sauté, mustard cream, crispy leeks"
+  ),
+  new Item(
+    `${beef}`,
+    "Beef brisket",
+    "Wild mushroom ragout, cheddar potato latke, ratatouille, artichoke gremolata, blue cheese, miso tomato ketchup"
+  ),
+];
+
 const createMenu = () => {
   const content = document.querySelector("#content");
 
-  const menuBox = document.createElement("div");
-  menuBox.classList.add("menubox");
-  menuBox.innerHTML = "HELLLLO";
+  const menuContainer = document.createElement("div");
+  menuContainer.classList.add("menucontainer");
 
-  //Card 1
-  const card1 = document.createElement("div");
-  const img1 = new Image();
-  const heading1 = document.createElement("h3");
-  const descr1 = document.createElement("p");
-  heading1.innerHTML = "Shrimp Corndog";
-  descr1.innerHTML = "Bistro honey mustard";
-  card1.appendChild(img1);
-  card1.appendChild(heading1);
-  card1.appendChild(descr1);
+  const divContainer = document.createElement("div");
+  divContainer.classList.add("divcontainer");
 
-  //Card 2
-  const card2 = document.createElement("div");
-  const img2 = new Image();
-  const heading2 = document.createElement("h3");
-  const descr2 = document.createElement("p");
-  heading2.innerHTML = "Bacon-wrapped apricots";
-  descr2.innerHTML =
-    "tequila-soaked apricots, hickory smoked bacon, jalapeño kumquat, honey glaze";
-  card2.appendChild(img2);
-  card2.appendChild(heading2);
-  card2.appendChild(descr2);
+  menu.forEach((item) => {
+    const card = document.createElement("div");
 
-  //Card 3
-  const card3 = document.createElement("div");
-  const img3 = new Image();
-  const heading3 = document.createElement("h3");
-  const descr3 = document.createElement("p");
-  heading3.innerHTML = "Cauliflower piccata";
-  descr3.innerHTML =
-    "Lemon caper aioli, pine nuts, parsley, pickled carrots, parmesan crisp";
-  card3.appendChild(img3);
-  card3.appendChild(heading3);
-  card3.appendChild(descr3);
+    const img = new Image();
+    img.src = item.image;
+    img.classList.add("image");
 
-  //Card 4
-  const card4 = document.createElement("div");
-  const img4 = new Image();
-  const heading4 = document.createElement("h3");
-  const descr4 = document.createElement("p");
-  heading4.innerHTML = "Shrimp Corndog";
-  descr4.innerHTML = "Bistro honey mustard";
-  card4.appendChild(img4);
-  card4.appendChild(heading4);
-  card4.appendChild(descr4);
+    const name = document.createElement("h3");
+    name.classList.add("name");
+    name.innerHTML = `${item.name}`;
 
-  //Card 5
-  const card5 = document.createElement("div");
-  const img5 = new Image();
-  const heading5 = document.createElement("h3");
-  const descr5 = document.createElement("p");
-  heading5.innerHTML = "Shrimp Corndog";
-  descr5.innerHTML = "Bistro honey mustard";
-  card5.appendChild(img5);
-  card5.appendChild(heading5);
-  card5.appendChild(descr5);
+    const descr = document.createElement("p");
+    descr.classList.add("descr");
+    descr.innerHTML = `${item.descr}`;
 
-  //Card 6
-  const card6 = document.createElement("div");
-  const img6 = new Image();
-  const heading6 = document.createElement("h3");
-  const descr6 = document.createElement("p");
-  heading6.innerHTML = "Shrimp Corndog";
-  descr6.innerHTML = "Bistro honey mustard";
-  card6.appendChild(img6);
-  card6.appendChild(heading6);
-  card6.appendChild(descr6);
+    card.appendChild(img);
+    card.appendChild(name);
+    card.appendChild(descr);
 
-  menuBox.appendChild(card1);
-  menuBox.appendChild(card2);
-  menuBox.appendChild(card3);
-  menuBox.appendChild(card4);
-  menuBox.appendChild(card5);
-  menuBox.appendChild(card6);
+    divContainer.appendChild(card);
+  });
 
-  content.appendChild(menuBox);
+  menuContainer.appendChild(divContainer);
+  content.appendChild(menuContainer);
 };
 
 export default createMenu;
